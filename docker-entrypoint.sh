a1a631bb371d0fa37f3234313d59284f16395d96
Madhu1512 <madhukar.thota@gmail.com>
2017-02-15 20:06:40 -0500

    Create docker-entrypoint.sh

---
+++ docker-entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+params=""
+if [ "$1" = 'curator_cli' ]; then
+  [ ! -z "$SSL_ENABLED" ] && params='--use_ssl --ssl-no-validate'
+  [ ! -z "$ELASTICSEARCH_USER_PASS" ] && params="${params} --http_auth ${ELASTICSEARCH_USER_PASS}"
+  [ ! -z "$ELASTICSEARCH_HOST" ] && params="${params} --host ${ELASTICSEARCH_HOST}"
+  [ ! -z "$ELASTICSEARCH_PORT" ] && params="${params} --port ${ELASTICSEARCH_PORT}"
+  shift
+        set -- "$@"
+  $(echo curator_cli "${params}" "$@")
+fi
+
+if [ "$1" = 'bash' ]; then
+  shift
+        set -- "$@"
+  eval "$@"
+fi
