3ee0df42e5cc2b3bf5d54c676718ffc2cefbf7b1
Madhu1512 <madhukar.thota@gmail.com>
2017-02-15 21:20:17 -0500

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 
 params=""
 if [ "$1" = 'curator_cli' ]; then
-  [ ! -z "$SSL_ENABLED" ] && params='--use_ssl --ssl-no-validate'
+  [ ! -z "$SSL_ENABLED" ] && params='--use_ssl'
   [ ! -z "$ELASTICSEARCH_USER_PASS" ] && params="${params} --http_auth ${ELASTICSEARCH_USER_PASS}"
   [ ! -z "$ELASTICSEARCH_HOST" ] && params="${params} --host ${ELASTICSEARCH_HOST}"
   [ ! -z "$ELASTICSEARCH_PORT" ] && params="${params} --port ${ELASTICSEARCH_PORT}"
